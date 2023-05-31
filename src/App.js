41d63850127f7820ce62a3ad5f6ea10f18394ec0
NerdNZ <eugene.rayner@gentrack.com>
2023-05-31 12:38:22 +1200

    changed to Ant design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-    </div>
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
   );
 }
 
